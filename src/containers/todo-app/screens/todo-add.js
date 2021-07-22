fc06de6f0915ab1261fa5084d5d7f11977d19d78
Aurelien <aurelien.strimelle@bstorm.be>
2021-07-22 16:12:10 +0200

    Ajout d'une nouvelle tache

--- src/containers/todo-app/screens/todo-add.js
+++ src/containers/todo-app/screens/todo-add.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../../../store/actions/todo-action';
 
 const TodoAdd = () => {
 
+    const [name, setName] = useState('');
+    const [desc, setDesc] = useState('');
+    const dispatch = useDispatch();
+    const nav = useNavigation();
+
+    const onTaskAdd = () => {
+        // TODO : Replace date by DatePicker => https://github.com/react-native-datetimepicker/datetimepicker
+        const now = new Date();
+
+        const task = {
+            name,
+            desc,
+            limitDate: new Date(now.getFullYear(), now.getMonth(), now.getDate() + 10)
+        }
+
+        // Envois de la tache dans le store
+        dispatch(addTask(task));
+
+        // Retour sur la page de la liste
+        nav.goBack();
+    }
+
     return (
         <View>
-            <Text>Todo Add</Text>
+            <Text>Nom de la tache</Text>
+            <TextInput value={name} onChangeText={text => setName(text)}
+                        underlineColorAndroid='#0FF' />
+            <Text>Description</Text>
+            <TextInput value={desc} onChangeText={text => setDesc(text)} 
+                        underlineColorAndroid='#0FF' />
+
+            <Button title='Ajouter'
+                    onPress={onTaskAdd} />
         </View>
     );
 }
--- src/containers/todo-app/screens/todo-list.js
+++ src/containers/todo-app/screens/todo-list.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {View, FlatList} from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import TaskItem from '../../../components/task-item/task-item';
+import { FAB } from 'react-native-elements';
 
 import {finishTask, deleteTask} from './../../../store/actions/todo-action';
+import { useNavigation } from '@react-navigation/native';
 
 const TodoList = () => {
 
     const todos = useSelector((state) => state.todoApp.todos);
     const dispatch = useDispatch();
+    const nav = useNavigation();
 
     const renderItem = ({item}) => (
         <TaskItem {...item} 
@@ -16,13 +19,23 @@ const TodoList = () => {
             onTaskFinish={(id) => dispatch(finishTask(id))} />
     )
 
+    const openScreenAdd = () => {
+        nav.navigate('todo/add');
+    }
+
     return (
-        <View>
+        <View style={{flex:1}}>
             <FlatList
                 data={todos}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
                 />
+            <FAB 
+                title='Add'
+                placement='right'
+                size='small'
+                color='#00FFFF'
+                onPress={openScreenAdd} />
         </View>
     );
 }
